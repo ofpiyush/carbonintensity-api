865eda74bc1f2d32a392850189f09bfe4eb00bc9
Piyush <piyush@codeitout.com>
2020-11-21 13:58:28 +0530

    improve readme

--- app.py
+++ app.py
@@ -29,34 +29,44 @@ kgco2e_per_mwh = {
 }
 
 
+def error(msg, code):
+    return jsonify({"error":msg}), code
 
 @app.route('/carbon-intensity/latest')
 def get_co2eq_per_kwh():
-    zone = request.args.get("zone")
 
-    # Production seems to be more universally available.
-    # If you want a more accurate and reliable results, please use the paid API from https://api.electricitymap.org
-    parser = PARSER_KEY_TO_DICT["production"][zone]
+    zone = request.args.get("zone", "")
 
-    # TODO: cache and/or save to database, right now we'll end up bombarding well meaning public APIs.
-    # The current assumption is that the users will only call the endpoint once every 10-20 minutes, so this is fine.
-    res = parser(zone, logger=logging.getLogger(__name__))
-    if  isinstance(res, (list, tuple)):
-        # TODO: fix this to use all available data
-        # At the time of writing this, I am only trying to get a working API with "close enough data"
-        res = res[0]
+    try:
+        # Production seems to be more universally available.
+        # If you want a more accurate and reliable results, please use the paid API from https://api.electricitymap.org
+        parser = PARSER_KEY_TO_DICT["production"][zone]
+    except KeyError as e:
+        return error("zone not found", 400)
+
+    try:
+        # TODO: cache and/or save to database, right now we'll end up bombarding well meaning public APIs.
+        # The current assumption is that the users will only call the endpoint once every 10-20 minutes, so this is fine.
+        res = parser(zone, logger=logging.getLogger(__name__))
+        if  isinstance(res, (list, tuple)):
+            # TODO: fix this to use all available data
+            # At the time of writing this, I am only trying to get a working API with "close enough data"
+            res = res[0]
+    except Exception as e:
+        return error("error fetching carbon intensity", 500)
+
     try:
         validate_production(res,zone)
     except ValidationError as e:
         # Log and respond with error
-        pass
+        return error("validation error: {}".format(str(e)), 500)
     
     co2eq = 0.0
     total_mw = 0.0
     default_co2e_per_mwh = kgco2e_per_mwh['unknown']
     for (source_type, mw) in res['production'].items():
         # Sum of (emission per megawatt by source * megawatts generated)
-        co2eq += (kgco2e_per_mwh.get(source_type,default_co2e_per_mwh) * mw)
+        co2eq += (kgco2e_per_mwh.get(source_type, default_co2e_per_mwh) * mw)
         total_mw += mw
 
     # Dividing co2eq by total_mw will give us kgco2eq/MW.
@@ -67,7 +77,10 @@ def get_co2eq_per_kwh():
         "zone": zone,
         "carbonIntensity": math.ceil(co2eq/total_mw),
     }
+
     if "datetime" in res:
+        # While python technically follows only ISO 8601 and seems rfc3339-compatible
+        # api.electricitymap.com seems to use the Z notation instead of the +00:00 and we don't want to trip anything up
         timestring = res["datetime"].astimezone(timezone('UTC')).isoformat()[:-6] + 'Z'
         response["datetime"] = timestring
         response["updatedAt"] = timestring
